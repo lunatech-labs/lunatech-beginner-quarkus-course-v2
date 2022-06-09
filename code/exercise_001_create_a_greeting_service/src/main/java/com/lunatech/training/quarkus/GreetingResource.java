63842c28f049ce4166bf445d875d1a5c7ca132df
codergo7 <codergo7@gmail.com>
2022-06-09 14:32:11 +0200

    Exercises 0 & 1 passing tests

--- code/exercise_001_create_a_greeting_service/src/main/java/com/lunatech/training/quarkus/GreetingResource.java
+++ code/exercise_001_create_a_greeting_service/src/main/java/com/lunatech/training/quarkus/GreetingResource.java
@@ -1,12 +1,7 @@
 package com.lunatech.training.quarkus;
 
-import io.quarkus.qute.Template;
-import io.quarkus.qute.TemplateInstance;
-
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
