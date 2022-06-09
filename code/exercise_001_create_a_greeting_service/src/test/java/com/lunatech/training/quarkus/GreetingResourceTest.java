89ff8fb4dbba9a12e52b85eb1cea282c732b0e49
codergo7 <codergo7@gmail.com>
2022-06-09 11:04:57 +0200

    Add full tests to exercise 1

--- code/exercise_001_create_a_greeting_service/src/test/java/com/lunatech/training/quarkus/GreetingResourceTest.java
+++ code/exercise_001_create_a_greeting_service/src/test/java/com/lunatech/training/quarkus/GreetingResourceTest.java
@@ -1,21 +1,37 @@
 package com.lunatech.training.quarkus;
 
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.MediaType;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@TestHTTPEndpoint(GreetingResource.class)
 public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
                 .when()
-                .get("/hello")
+                .get()
                 .then()
                 .statusCode(200)
+                .contentType(MediaType.TEXT_PLAIN)
                 .body(is("Hello, Quarkians!"));
+    }
+
+    @Test
+    public void testHelloSubjectEndpoint() {
+        given()
+                .when()
+                .get("/world")
+                .then()
+                .statusCode(200)
+                .contentType(MediaType.TEXT_HTML)
+                .body("html.body.h1", is("Hello world!"));
     }
 }
