79a4c8ce422a526061a4d1b0dade1bc94f1668c6
codergo7 <codergo7@gmail.com>
2022-06-09 16:04:32 +0200

    Update solution for exercise 1 and add a test for it

--- code/exercise_001_create_a_greeting_service/src/main/java/com/lunatech/training/quarkus/GreetingResource.java
+++ code/exercise_001_create_a_greeting_service/src/main/java/com/lunatech/training/quarkus/GreetingResource.java
@@ -2,6 +2,7 @@ package com.lunatech.training.quarkus;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,5 +13,12 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello, Quarkians!";
+    }
+
+    @Path("{subject}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloSubject(@PathParam("subject") String subject) {
+        return "Hello " + subject;
     }
 }
