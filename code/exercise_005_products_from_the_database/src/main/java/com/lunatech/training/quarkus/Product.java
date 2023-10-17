3f78d6634ba33cca7ede053b77055710d0c88985
dix <francois.dixmeras@lunatech.fr>
2023-10-17 12:14:04 +0200

    Exercice 5 : adapt entity and import statement for Hibernate ORM 6

--- code/exercise_005_products_from_the_database/src/main/java/com/lunatech/training/quarkus/Product.java
+++ code/exercise_005_products_from_the_database/src/main/java/com/lunatech/training/quarkus/Product.java
@@ -1,18 +1,24 @@
 package com.lunatech.training.quarkus;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
 @Entity
-public class Product extends PanacheEntity {
+public class Product extends PanacheEntityBase {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
     public String name;
     public String description;
     public BigDecimal price;
 
-    public Product(){}
+    public Product() {}
 
     public Product(Long id, String name, String description, BigDecimal price) {
         this.id = id;
