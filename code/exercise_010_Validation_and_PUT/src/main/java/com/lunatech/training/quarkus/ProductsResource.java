0d7d4bf2e4e1cb57599bb87629820566949c0308
Neil Benn <neil.benn@lunatech.fr>
2024-06-14 00:29:25 +0200

    Remove the @Blocking annotation from ProductsResource

--- code/exercise_010_Validation_and_PUT/src/main/java/com/lunatech/training/quarkus/ProductsResource.java
+++ code/exercise_010_Validation_and_PUT/src/main/java/com/lunatech/training/quarkus/ProductsResource.java
@@ -1,7 +1,5 @@
 package com.lunatech.training.quarkus;
 
-import io.smallrye.common.annotation.Blocking;
-
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
@@ -14,14 +12,12 @@ import java.util.List;
 public class ProductsResource {
 
     @GET
-    @Blocking
     public List<Product> products() {
         return Product.listAll();
     }
 
     @GET
     @Path("{productId}")
-    @Blocking
     public Product details(@PathParam("productId") Long productId) {
         Product product = Product.findById(productId);
         if (product != null) {
