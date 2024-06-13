0968b6cb1414d6f7c648532af2e5a7ba5bc1d268
Neil Benn <neil.benn@lunatech.fr>
2024-06-14 01:14:04 +0200

    Add more test cases to ProductsResourceTest

--- code/exercise_011_Going_Reactive/src/test/java/com/lunatech/training/quarkus/ProductsResourceTest.java
+++ code/exercise_011_Going_Reactive/src/test/java/com/lunatech/training/quarkus/ProductsResourceTest.java
@@ -2,15 +2,21 @@ package com.lunatech.training.quarkus;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@TestMethodOrder(OrderAnnotation.class)
 public class ProductsResourceTest {
 
     @Test
+    @Order(1)
     public void testProductsEndpoint() {
         given()
                 .when().get("/products")
@@ -21,6 +27,7 @@ public class ProductsResourceTest {
     }
 
     @Test
+    @Order(2)
     public void testProductDetailsEndpoint() {
         given()
                 .when().get("/products/1")
@@ -31,9 +38,48 @@ public class ProductsResourceTest {
     }
 
     @Test
+    @Order(3)
     public void testProductDetailsEndpointNotFound() {
         given()
                 .when().get("/products/11")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    @Order(4)
+    public void testProductUpdateEndpoint() {
+        String updateRequestBody = """
+            {
+                "name" : "Chair",
+                "description" : "A steel frame chair, with oak seat",
+                "price" : 59.95
+            }""";
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(updateRequestBody)
+                .when().put("/products/1")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("description", is("A steel frame chair, with oak seat"));
+    }
+
+    @Test
+    @Order(5)
+    public void testProductUpdateEndpointNotFound() {
+        String updateRequestBody = """
+            {
+                "name" : "Chair",
+                "description" : "A steel frame chair, with oak seat",
+                "price" : 69.95
+            }""";
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(updateRequestBody)
+                .when().put("/products/11")
                 .then()
                 .statusCode(404);
     }
